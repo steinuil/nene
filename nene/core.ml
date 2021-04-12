fe998df158197231d03d78afbbad480acca34c3c
steinuil <steenuil.owl@gmail.com>
2021-04-13 00:37:50 +0200

    deleted old rss parsing

--- nene/core.ml
+++ nene/core.ml
@@ -1,53 +1,12 @@
 open Types
 
-exception Invalid_feed
-
-let get_torrent_info attrs =
-  let rec loop =
-    let open Xml in
-    function
-    | _, (Some filename, Some link) -> { filename; link }
-    | Element ("title", _, [ PCData name ]) :: rest, (_, l) ->
-        loop (rest, (Some name, l))
-    | Element ("link", _, [ PCData link ]) :: rest, (n, _) ->
-        loop (rest, (n, Some link))
-    | _ :: rest, x -> loop (rest, x)
-    | [], _ -> raise Invalid_feed
-  in
-  loop (attrs, (None, None))
-
-let torrents_of_rss_doc doc =
-  let open Xml in
-  let filter_items acc = function
-    | Element ("item", _, children) -> get_torrent_info children :: acc
-    | _ -> acc
-  in
-  match parse_string doc with
-  | Element ("rss", _, [ Element ("channel", _, children) ]) ->
-      List.fold_left filter_items [] children
-  | exception Error _ -> raise Invalid_feed
-  | exception Dtd.Parse_error _ -> raise Invalid_feed
-  | _ -> raise Invalid_feed
-
-let parse_episode_filename regexp filename =
-  try
-    let number = int_of_string @@ Str.replace_first regexp "\\1" filename in
-    let version =
-      try
-        let ver = Str.replace_first regexp "\\2" filename in
-        int_of_string @@ String.sub ver 1 (String.length ver - 1)
-      with Failure _ -> 1
-    in
-    Some { number; version }
-  with Failure _ -> None
-
 let is_episode_new seen_eps Episode_num.{ number; version } =
   match IntMap.find_opt number seen_eps with
   | None -> true
   | Some last_ver -> version > last_ver
 
-let new_episodes pattern (seen_eps, new_links) { filename; link } =
-  match Pattern.parse_episode pattern filename with
+let new_episodes pattern (seen_eps, new_links) Config.{ title; link } =
+  match Pattern.parse_episode pattern title with
   | None -> (seen_eps, new_links)
   | Some ep ->
       if is_episode_new seen_eps ep then
@@ -92,16 +51,6 @@ let die_fatal str =
   exit 1
 
 let run ~(trackers : Config.tracker list) ~seen_file ~jobs:_ ~backend:_ =
-  (* let trackers =
-       try Save.load_trackers shows_file with
-       | Sys_error err -> die_fatal err
-       | Failure _ -> die_fatal (shows_file ^ ": Unbalanced left parenthesis")
-       | Sexplib.Sexp.Parse_error { err_msg; _ } ->
-           die_fatal (Printf.sprintf "%s: %s" shows_file err_msg)
-       | Sexplib.Conv.Of_sexp_error
-           (Sexplib.Sexp.Annotated.Conv_exn (loc, Failure err), _) ->
-           die_fatal (Printf.sprintf "%s: %s " loc err)
-     in *)
   let seen = Save.load_seen seen_file in
   total := List.length trackers;
   let%lwt new_seen =
@@ -110,17 +59,16 @@ let run ~(trackers : Config.tracker list) ~seen_file ~jobs:_ ~backend:_ =
            (* Download and parse RSS documents *)
            let%lwt doc = Config.download (Uri.to_string rss_url) in
            let%lwt torrents =
-             try
-               let t = torrents_of_rss_doc doc in
-               Lwt.return t
-             with Invalid_feed ->
-               let%lwt () =
-                 error_print
-                   (Printf.sprintf
-                      "\x1b[1m%s\x1b[0m returned an invalid document"
-                      (Uri.to_string rss_url))
-               in
-               Lwt.return []
+             match Rss.torrents_from_string doc with
+             | Some t -> Lwt.return t
+             | None ->
+                 let%lwt () =
+                   error_print
+                     (Printf.sprintf
+                        "\x1b[1m%s\x1b[0m returned an invalid document"
+                        (Uri.to_string rss_url))
+                 in
+                 Lwt.return []
            in
            incr count;
            let%lwt () = print_status () in
